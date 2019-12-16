7fb634e344ccf7041491115fc3d9532d552d0b1f
kamoshibito510 <kamoshibito510@icloud.com>
2019-12-16 23:31:21 +0900

    要らないルーティング発見

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
   get 'mypage', to: 'mypages#index'
-  get 'index', to: 'users#index'
+  get 'index', to: 'users#index'  #←←このルーティングは要らないので消してよし
   resource :products
   #get 'users', to: 'users#new' 
   #get 'login', to: 'users#login-new' #仮ルーティング、ログイン画面用のビュー
@@ -27,11 +27,11 @@ Rails.application.routes.draw do
     end
   end
   get 'profile', to: 'mypages#edit' #仮ルーティング、プロフィール編集画面
-　get 'identification', to: 'mypages#identification' #仮ルーティング、ユーザー本人確認画面
+  get 'identification', to: 'mypages#identification' #仮ルーティング、ユーザー本人確認画面
   get 'logout', to: 'users#logout' #仮ルーティング、ログアウト画面用のビュー
   get 'buy', to: 'users#buy' #仮ルーティング、商品購入確認ページ
   get 'card', to: 'users#card' #仮ルーティング、クレジットカード登録ページ
   get 'sell', to: 'products#sell' #仮ルーティング、商品出品ページ
   get 'detail', to: 'products#detail' #仮ルーティング、商品詳細ページ
-#   get 'card', to: 'users#card' #仮ルーティング、クレジットカード登録ページ
+  # get 'card', to: 'users#card' #仮ルーティング、クレジットカード登録ページ
 end
