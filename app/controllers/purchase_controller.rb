cd98e94e443653a89d01eed84e1a21441871e6d1
kamoshibito510 <kamoshibito510@icloud.com>
2020-01-10 19:44:30 +0900

    ユーザーの情報を取得し購入画面で表示させる。

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,6 +3,7 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
+    @user = current_user
     card = Card2.where(user_id: current_user.id).first
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
@@ -17,7 +18,25 @@ class PurchaseController < ApplicationController
     end
   end
 
+  def done
+    @user = current_user
+    card = Card2.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card2s", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+
+  end
+
   def pay
+    binding.pry
     card = Card2.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
