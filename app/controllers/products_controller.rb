fc3e4c11a835aaafa7a8252523a5bce6a9872fff
kamoshibito510 <kamoshibito510@icloud.com>
2020-02-02 13:44:18 +0900

    カテゴリーのリンク先ページ追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,16 @@ class ProductsController < ApplicationController
     @parents = Category.roots.order("id ASC").limit(13)
   end
 
+  def category
+    @ladies = Sellitem.includes(:images).where(category_id: [*1..137]).order("created_at DESC").limit(10)
+    @mens= Sellitem.includes(:images).where(category_id: [*138..360]).order("created_at DESC").limit(10)
+    @appliances = Sellitem.includes(:images).where(category_id: [*360..775]).order("created_at DESC").limit(10)
+    @toys = Sellitem.includes(:images).where(category_id: [*776..1000]).order("created_at DESC").limit(10)
+    @parents = Category.roots.order("id ASC").limit(13)
+    @categories = Category.roots.find_by(id: params[:id])
+    
+  end
+
   def new
   end
 
