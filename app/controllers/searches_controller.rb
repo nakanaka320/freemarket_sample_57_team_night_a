df0a4e03505421ee5c0f69e8b928c436fa007a28
nakanaka320 <nakanakamitsuo@icloud.com>
2020-02-02 16:04:44 +0900

    修正　５

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,11 @@ class SearchesController < ApplicationController
   def index
     @sellitems = Sellitem.search(params[:search]).limit(132)
     @search = params[:search]
+    @parents = Category.all.order("id ASC").limit(13)
+  end
+
+  def new
+    @parents = Category.all.order("id ASC").limit(13)
   end
 
 end
