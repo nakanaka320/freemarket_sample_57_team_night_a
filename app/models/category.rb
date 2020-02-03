2e33857a3e9ce84c7fcc6f3fa8e0fe7ad0b765a1
nakanaka320 <nakanakamitsuo@icloud.com>
2020-02-03 20:44:50 +0900

    修正６

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,4 @@ class Category < ApplicationRecord
   has_many :sellitems
   has_many :buyitems
   has_ancestry
-
-  # belongs_to :parent, class_name: :Category  
-  # has_many :children, class_name: :Category, foreign_key: :ancestry
 end
