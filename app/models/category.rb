7d7688485f84d719247ce57c798d9dd1ceda3656
nakanaka320 <nakanakamitsuo@icloud.com>
2020-02-03 12:45:34 +0900

    修正　５

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ApplicationRecord
   has_many :buyitems
   has_ancestry
 
-  belongs_to :parent, class_name: :Category  
-  has_many :children, class_name: :Category, foreign_key: :ancestry
+  # belongs_to :parent, class_name: :Category  
+  # has_many :children, class_name: :Category, foreign_key: :ancestry
 end
